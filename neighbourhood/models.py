9bed3b2fc34643b1e7381b22cb85a0b9b9d35caf
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-09-14 14:09:42 +0300

    created class neighbourhood which is linked to the user

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -3,6 +3,19 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
 # Create your models here.
+class neighbourhood(models.Model):
+    neighbourhood= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.neighbourhood
+
+    def save_neighbourhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls,neighbourhood):
+        cls.objects.filter(neighbourhood=neighbourhood).delete()
+
 
 class Profile(models.Model):
     profpic = models.ImageField(upload_to='profpics/')
