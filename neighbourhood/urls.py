58a4641d47a2060eba5a63c70c857d1392955eeb
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-09-14 12:45:48 +0300

    linked the profile

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url(r'^my-profile/',views.my_profile, name='my-profile'),
+    url(r'^user/(?P<username>\w{0,50})',views.user_profile,name='user-profile'),
+    url(r'^create/profile$',views.create_profile, name='create-profile'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
