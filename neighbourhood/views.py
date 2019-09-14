21e63d574bca381ca43e6297db752d6cb3562a37
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-09-14 13:05:30 +0300

    create prfile working perfectly

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
+from .forms import *
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Neighbourhood')
