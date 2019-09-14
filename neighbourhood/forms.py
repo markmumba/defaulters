3d773c08cd34eca228ae09a8b65627bcb231552f
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-09-14 15:04:00 +0300

    created a new blogpost form

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -5,3 +5,9 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model=Profile
         exclude=['username']
+    
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model=BlogPost
+        exclude=['username','neighbourhood','profpic']
