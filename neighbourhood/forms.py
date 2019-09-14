c57bce2081b440a8619dd65435432b1cd1b14329
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-09-14 17:19:18 +0300

    registered the business model in the admin panel and defined the business form inside forms.py

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -5,7 +5,7 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model=Profile
         exclude=['username']
-    
+        
 
 class BlogPostForm(forms.ModelForm):
     class Meta:
@@ -16,3 +16,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model=Comment
         exclude=['username','post']
+    
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        exclude=['owner','neighbourhood']
