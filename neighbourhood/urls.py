26983e3441f3f18f7b7aa24bbe8c2df9c074feda
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-09-14 15:11:43 +0300

    defined the url for creating a new blog

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -8,7 +8,8 @@ urlpatterns=[
     url(r'^my-profile/',views.my_profile, name='my-profile'),
     url(r'^user/(?P<username>\w{0,50})',views.user_profile,name='user-profile'),
     url(r'^create/profile$',views.create_profile, name='create-profile'),
-    url(r'^update/profile$',views.update_profile, name='update-profile') 
+    url(r'^update/profile$',views.update_profile, name='update-profile'),
+    url(r'^new/blogpost$',views.new_blogpost, name='new-blogpost'), 
 ]
 
 if settings.DEBUG:
