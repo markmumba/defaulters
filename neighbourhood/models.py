83aafe93bb9a0835b805d310f83dff39618c5f0f
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-09-14 14:55:09 +0300

    added class blog

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -3,27 +3,28 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
 # Create your models here.
-class neighbourhood(models.Model):
-    neighbourhood= models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.neighbourhood
-
-    def save_neighbourhood(self):
-        self.save()
-
-    @classmethod
-    def delete_neighbourhood(cls,neighbourhood):
-        cls.objects.filter(neighbourhood=neighbourhood).delete()
-
 
 class Profile(models.Model):
     profpic = models.ImageField(upload_to='profpics/')
     description = HTMLField()
-    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    # neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
     username = models.ForeignKey(User,on_delete=models.CASCADE)
     name =models.CharField(max_length=100)
     email = models.EmailField()
 
     def __str__(self):
         return self.name
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='post/')
+    post = HTMLField()
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    # neighbourhood= models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
+    profpic = models.ImageField(upload_to='profpics/')
+
+    def __str__(self):
+        return self.title
+    
+
