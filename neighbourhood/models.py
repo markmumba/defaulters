655bd743bbe164b78b157a9735178e7bc30b2d9c
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-09-14 17:07:25 +0300

    created class Business and defined it's relationship with other tables

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -47,3 +47,17 @@ class Comment(models.Model):
     username = models.ForeignKey(User,on_delete=models.CASCADE)
     post = models.ForeignKey(BlogPost,on_delete=models.CASCADE)
 
+class Business(models.Model):
+    logo = models.ImageField(upload_to='businesslogo/')
+    description = HTMLField()
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+    address =models.CharField(max_length=100)
+    contact = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
