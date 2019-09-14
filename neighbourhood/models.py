f419cb159f6e5f67bcc0a7890a7837cdd5d088a9
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-09-14 19:55:30 +0300

    created class authorities and defined the respective fields

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -87,3 +87,13 @@ class Health(models.Model):
         return self.name
 
 
+class Authorities(models.Model):
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+    contact = models.IntegerField()
+    address =models.CharField(max_length=100)
+
+    def __str__(self):
+    return self.name
+
