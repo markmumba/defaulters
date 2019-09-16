01ffa49553a59ab13ac377d85096c4a3dc7d3280
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-09-16 08:11:56 +0300

    the tests ran successfully

--- neighbourhood/tests.py
+++ neighbourhood/tests.py
@@ -13,32 +13,12 @@ class neighbourhoodTestClass(TestCase):
     def tearDown(self):
         neighbourhood.objects.all().delete()
 
-    def test_create_method(self):
+    def test_save_method(self):
         self.Kasarani.create_neighbourhood()
         hood = neighbourhood.objects.all()
         self.assertTrue(len(hood)>0)
 
-    def test_delete_method(self):
-        self.Kasarani.delete_neighbourhood('Kasarani')
-        hood = neighbourhood.objects.all()
-        self.assertTrue(len(hood)==0)
-
-# class healthservicesTestClass(TestCase):
-#     def setUp(self):
-#         self.Radiotherapy = healthservices(healthservices='Radiotherapy')
-
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.Radiotherapy,healthservices))
-
-#     def tearDown(self):
-#         healthservices.objects.all().delete()
-
-#     def test_save_method(self):
-#         self.Radiotherapy.save_healthservices()
-#         health = healthservices.objects.all()
-#         self.assertTrue(len(health)>0)
-
-#     def test_delete_method(self):
-#         self.Radiotherapy.delete_healthservices('Radiotherapy')
-#         health = healthservices.objects.all()
-#         self.assertTrue(len(health)==0)
+    # def test_delete_method(self):
+    #     self.Kasarani.delete_neighbourhood('Kasarani')
+    #     hood = neighbourhood.objects.all()
+	# 	self.assertTrue(len(hood) == 0)
